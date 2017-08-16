7512a9c264dd591420fd3a05dd944f4db87ea8cf
wanglijun93 <1402499306@qq.com>
2017-08-16 22:43:47 +0800

    Java经典算法之 冒泡排序

--- app/src/main/java/wanglijun/vip/practice/MainActivity.java
+++ app/src/main/java/wanglijun/vip/practice/MainActivity.java
@@ -1,13 +1,29 @@
 package wanglijun.vip.practice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mSortMaoPao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSortMaoPao = (Button) findViewById(R.id.sortMaoPao);
+        mSortMaoPao.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sortMaoPao:
+                SortMaoPao.SamllAndBig();
+                break;
+            default:
+        }
     }
 }
---
+++ app/src/main/java/wanglijun/vip/practice/SortMaoPao.java
@@ -0,0 +1,32 @@
+package wanglijun.vip.practice;
+
+import wanglijun.vip.practice.utils.LogUtils;
+
+/**
+ * @author： wlj
+ * @Date: 2017-08-16
+ * @email: wanglijundev@gmail.com
+ * @desc: java算法之 冒泡排序
+ */
+
+public class SortMaoPao {
+    /**
+     * 从小到大
+     */
+    public static  void SamllAndBig() {
+        int arr[] = {6, 3, 1, 9, 2, 35, 11};
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp;
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            LogUtils.d(" " + arr[i] +" ");
+        }
+    }
+}
